ce0a021ed0bb71a92cb8baedc9c0803235b9915d
Yusuf Arslan <yusufarsln98@outlook.com>
2023-04-12 20:50:22 +0300

    practicing...

---
+++ CSE344 - System Programming/Coding Practices/practice37.c
@@ -0,0 +1,79 @@
+// Implementing System() function
+
+/*
+    We need to use fork() to create a child that then does an execl() to
+    to implement a command like:
+
+    execl("/bin/sh", "sh", "-c", command, (char *)NULL);
+
+    To collect the status of the child created by system(), we use a waitpid() call that
+    specifies the child's process ID.
+*/
+
+// Code written with ChatGPT - Similar with the code covered in the lecture
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+int system(char *command)
+{
+    pid_t pid;
+    int status;
+
+    if (command == NULL)
+    {
+        return -1;
+    }
+
+    pid = fork();
+
+    if (pid == -1)
+    {
+        return -1;
+    }
+
+    if (pid == 0)
+    { /* Child process */
+        /* Execute the command using execl */
+        execl("/bin/sh", "sh", "-c", command, (char *)NULL);
+
+        _exit(127); // not supposed to reach here
+    }
+    else
+    { /* Parent process */
+        /* Wait for the child to terminate */
+        if (waitpid(pid, &status, 0) == -1)
+        {
+            return -1;
+        }
+        /* Return the exit status of the child process */
+        return status;
+    }
+}
+
+int main()
+{
+    /* Call the my_system() function with the command to execute */
+    int result = my_system("ls");
+
+    /* Print the exit status of the command */
+    printf("Exit status: %d\n", result);
+
+    return 0;
+}
+
+/*
+    This my_system() function takes a string argument that contains the
+    command to be executed. It uses the fork() system call to create a new process,
+    and then uses the execl() system call to replace the child process
+    with the specified command.
+
+    The parent process waits for the child process to terminate using
+    the waitpid() system call, and then returns the exit status of the child
+    process using the WEXITSTATUS() macro.
+
+    Note that this implementation is a simplified version of the system()
+    function and does not handle signals or other edge cases that the actual
+    system() function would handle.
+*/
---
+++ CSE344 - System Programming/Coding Practices/practice38.c
@@ -0,0 +1,89 @@
+// More precise system() implementation from the lecture
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <signal.h>
+
+// add mask to the signal set
+
+int system(char *command)
+{
+    sigset_t block_mask, orig_mask;
+    struct sigaction sa_ignore, sa_orig_quit, sa_orig_int, sa_default;
+    pid_t child_pit;
+    int status, saved_errno;
+
+    if (command == NULL) // Is a shell available?
+    {
+        return system(":") == 0;
+    }
+
+    sigemptyset(&block_mask); // Block SIGCHLD
+    sigaddset(&block_mask, SIGCHLD);
+    sigprocmask(SIG_BLOCK, &block_mask, &orig_mask);
+
+    sa_ignore.sa_handler = SIG_IGN; // Ignore SIGINT and SIGQUIT
+    sa_ignore.sa_flags = 0;
+    sigemptyset(&sa_ignore.sa_mask);
+    sigaction(SIGINT, &sa_ignore, &sa_orig_int);
+    sigaction(SIGQUIT, &sa_ignore, &sa_orig_quit);
+
+    switch (child_pit = fork())
+    {
+    case -1: // fork() failed
+        status = -1;
+        break;
+    case 0:
+        sa_default.sa_handler = SIG_DFL;
+        sa_default.sa_flags = 0;
+        sigemptyset(&sa_default.sa_mask);
+
+        if (sa_orig_int.sa_handler != SIG_IGN)
+        {
+            sigaction(SIGINT, &sa_default, NULL);
+        }
+
+        if (sa_orig_quit.sa_handler != SIG_IGN)
+        {
+            sigaction(SIGQUIT, &sa_default, NULL);
+        }
+
+        sigprocmask(SIG_SETMASK, &orig_mask, NULL); // Restore signal mask
+        execl("/bin/sh", "sh", "-c", command, (char *)NULL);
+        _exit(127);
+    default:
+        while (waitpid(child_pit, &status, 0) == -1)
+        {
+            if (errno != EINTR)
+            {
+                status = -1;
+                break;
+            }
+        }
+        break;
+    }
+
+    // unblock SIGCHLD, restore dispositions of SIGINT and SIGQUIT
+
+    saved_errno = errno; // the following may change 'errno'
+    sigprocmask(SIG_SETMASK, &orig_mask, NULL);
+    sigaction(SIGINT, &sa_orig_int, NULL);
+    sigaction(SIGQUIT, &sa_orig_quit, NULL);
+    errno = saved_errno;
+
+    return status;
+}
+
+int main()
+{
+    /* Call the my_system() function with the command to execute */
+    int result = my_system("ls");
+
+    /* Print the exit status of the command */
+    printf("Exit status: %d\n", result);
+
+    return 0;
+}
